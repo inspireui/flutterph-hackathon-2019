e54a1fde21b366cc0f80a561165f7ea8916b738f
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-09-07 14:05:24 +0800

    Add initial setup for page view

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -14,23 +14,80 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class DashboardScreenState extends State<DashboardScreen> {
-  // TODO: Make this responsive
-  double _width = 1100;
+  Color _pageBackgroundColor = Color(0xfffefef3);
+  double _height = 0.0;
+  double _width = 0.0;
+
+  PageController _pageController;
+  double _currentPage = 0;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    );
+
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page;
+
+        if (_currentPage == 0) {
+          _pageBackgroundColor = Color(0xfffefef3);
+        } else if (_currentPage == 1) {
+          _pageBackgroundColor = Color(0xffffd0d2);
+        } else if (_currentPage == 2) {
+          _pageBackgroundColor = Color(0xff00ff00);
+        } else if (_currentPage == 3) {
+          _pageBackgroundColor = Color(0xff00ff00);
+        } else if (_currentPage == 4) {
+          _pageBackgroundColor = Color(0xff00ff00);
+        } else {
+          _pageBackgroundColor = Color(0xfffefef3);
+        }
+      });
+
+      print("Current page $_currentPage");
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.width;
+    _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 20, left: 20, right: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              buildHeaderView(),
-              buildStudyJamsView(),
-              buildMechanicsView(),
-              Padding(padding: EdgeInsets.only(bottom: 50)),
-              buildFooterView(),
-              Padding(padding: EdgeInsets.only(bottom: 50)),
-            ],
+      // body: Container(
+      //   padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: <Widget>[
+      //         buildHeaderView(),
+      //         buildStudyJamsView(),
+      //         buildMechanicsView(),
+      //         Padding(padding: EdgeInsets.only(bottom: 50)),
+      //         buildFooterView(),
+      //         Padding(padding: EdgeInsets.only(bottom: 50)),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 1000),
+          color: _pageBackgroundColor,
+          height: _height,
+          width: _width,
+          child: PageView(
+            scrollDirection: Axis.vertical,
+            controller: _pageController,
+            children: <Widget>[buildStudyJamsView(), buildMechanicsView()],
           ),
         ),
       ),
@@ -92,170 +149,227 @@ class DashboardScreenState extends State<DashboardScreen> {
   }
 
   Widget buildStudyJamsView() {
-    return Container(
-      width: _width,
-      child: Stack(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: 800,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Image.network(
-                    'https://user-images.githubusercontent.com/20706361/64428818-467b0180-d0e7-11e9-9f9d-c2fcb0a90619.png',
+    return Center(
+      child: Container(
+        width: _width / 1.25,
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 800,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Image.network(
+                      'https://user-images.githubusercontent.com/20706361/64428818-467b0180-d0e7-11e9-9f9d-c2fcb0a90619.png',
+                    ),
                   ),
                 ),
               ),
-            ],
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 50),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Material(
-                      elevation: 12.0,
-                      color: Colors.white,
-                      clipBehavior: Clip.antiAlias,
-                      shadowColor: Colors.black54,
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Container(
-                        height: 450,
-                        width: 500,
-                        padding: EdgeInsets.all(30),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              SectionHeaderWidget(title: 'Study Jams'),
-                              Padding(padding: EdgeInsets.only(bottom: 16)),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  FPHChip(
-                                    title: 'Manila',
-                                    color: Colors.blue,
-                                    isSelected: true,
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 50),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Material(
+                        elevation: 12.0,
+                        color: Colors.white,
+                        clipBehavior: Clip.antiAlias,
+                        shadowColor: Colors.black54,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: Container(
+                          height: 450,
+                          width: 500,
+                          padding: EdgeInsets.all(30),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SectionHeaderWidget(title: 'Study Jams'),
+                                Padding(padding: EdgeInsets.only(bottom: 16)),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    FPHChip(
+                                      title: 'Manila',
+                                      color: Colors.blue,
+                                      isSelected: true,
+                                    ),
+                                    FPHChip(
+                                      title: 'Cebu',
+                                      color: Colors.blue,
+                                      isSelected: false,
+                                    ),
+                                  ],
+                                ),
+                                Padding(padding: EdgeInsets.only(bottom: 24)),
+                                Text(
+                                  'September 24, 2019',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
                                   ),
-                                  FPHChip(
-                                    title: 'Cebu',
-                                    color: Colors.blue,
-                                    isSelected: false,
+                                ),
+                                Text(
+                                  'Everything is a Widget, Flutter Animations',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                ],
-                              ),
-                              Padding(padding: EdgeInsets.only(bottom: 24)),
-                              Text(
-                                'September 24, 2019',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.orange,
                                 ),
-                              ),
-                              Text(
-                                'Everything is a Widget, Flutter Animations',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                                Text(
+                                  'Venue',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                'Venue',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
+                                Padding(padding: EdgeInsets.only(bottom: 24)),
+                                Text(
+                                  'October 1, 2019',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
+                                  ),
                                 ),
-                              ),
-                              Padding(padding: EdgeInsets.only(bottom: 24)),
-                              Text(
-                                'October 1, 2019',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.orange,
+                                Text(
+                                  'State Management, RESTful APIs in Flutter',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                              ),
-                              Text(
-                                'State Management, RESTful APIs in Flutter',
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                'Venue',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black54,
+                                Text(
+                                  'Venue',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget buildMechanicsView() {
-    return Container(
-      width: _width,
-      child: Stack(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                width: 600,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Image.network(
-                      'https://user-images.githubusercontent.com/20706361/64430494-f9009380-d0ea-11e9-9513-16ef6c3db06f.png'),
-                ),
-              ),
-            ],
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 50),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Material(
-                      elevation: 12.0,
-                      color: Colors.white,
-                      clipBehavior: Clip.antiAlias,
-                      shadowColor: Colors.black54,
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Container(
-                        height: 400,
-                        width: 400,
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          children: <Widget>[],
-                        ),
-                      ),
+    return Center(
+      child: Container(
+        width: _width / 1.25,
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: 600,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Image.network(
+                      'https://user-images.githubusercontent.com/20706361/64430494-f9009380-d0ea-11e9-9513-16ef6c3db06f.png',
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 50),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Material(
+                        elevation: 12.0,
+                        color: Colors.white,
+                        clipBehavior: Clip.antiAlias,
+                        shadowColor: Colors.black54,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: Container(
+                          height: 450,
+                          width: 500,
+                          padding: EdgeInsets.all(30),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SectionHeaderWidget(
+                                    title: 'Registration / Mechanics'),
+                                Padding(padding: EdgeInsets.only(bottom: 16)),
+                                Padding(padding: EdgeInsets.only(bottom: 24)),
+                                Text(
+                                  'September 24, 2019',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                                Text(
+                                  'Everything is a Widget, Flutter Animations',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Venue',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                Padding(padding: EdgeInsets.only(bottom: 24)),
+                                Text(
+                                  'October 1, 2019',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                                Text(
+                                  'State Management, RESTful APIs in Flutter',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Venue',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
